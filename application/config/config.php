89c2f7df558374068033896c3ef2b59919d659c7
Dam Duy Tien <tiendd58@vnu.edu.vn>
2015-03-11 16:13:31 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/studentneed/';
+$config['base_url']	= 'http://localhost/CNPM/';
 
 /*
 |--------------------------------------------------------------------------
