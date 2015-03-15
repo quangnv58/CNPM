32535d98ebc397da49e9779e63a9042eee086aae
quangnv58 <quang0915057307@gmail.com>
2015-03-15 20:43:29 +0700

    update2
    phan quyen

--- application/views/frontend/components/tinmoi/tin.php
+++ application/views/frontend/components/tinmoi/tin.php
@@ -12,25 +12,45 @@
 		<tr>
     		<th>Tiêu đề</th>
     		<th>Ngày đăng</th>
-    		<th colspan="2">Quản lý</th>
+    		<?php
+			if($this->session->userdata('logged_in')!=NULL){
+    			echo'
+    			<th colspan="2">Quản lý</th>
+    			';
+    		}
+    		?>
+    		
 		</tr>
 <?php
+
 	foreach($post as $val){
 		
 ?>
 	<tr>
-		<td><a href="<?php echo "http://localhost/CNPM/hientin/show/".$val['idpost'] ?>">
+		<td><a href="<?php echo "http://localhost/studentneed/hientin/show/".$val['idpost'] ?>">
 				<h1><?php echo $val["title"]?></h1>
 			</a></td>
 		<td><?php echo $val['postdate']?></td>
-		<td width="44">
-			<div align="center">
-			  <a href="<?php echo "http://localhost/CNPM/suatin/edit/".$val['idpost'] ?>">
-   			  <img src="public/frontend/images/b_edit.png" data-toggle="tooltip" data-placement="left" title="edit"/>
-       			</a> 
-        	</div>
-		</td>	
-        <td width="41"><a href="<?php echo "http://localhost/CNPM/suatin/delete/".$val['idpost'] ?>"><img src="public/frontend/images/b_drop.png" data-toggle="tooltip" data-placement="left" title="delete" /></a></td>
+		<?php
+		$this->session->userdata('logged_in');
+		$session_data = $this->session->userdata('logged_in');
+		$user= $session_data['user'];
+		if($this->session->userdata('logged_in')!=NULL && $user==$val['belong']):?>
+					<td width="44">
+						<div align="center">
+						  <a href="<?php echo "http://localhost/studentneed/suatin/edit/".$val["idpost"] ?>">						  
+						  	<img src="public/frontend/images/b_edit.png" data-toggle="tooltip" data-placement="left" title="edit"/> 			  
+	       				  </a> 
+	        			</div>
+					</td>	
+       				<td width="41">
+	       				<a href="<?php echo "http://localhost/studentneed/suatin/delete/".$val["idpost"] ?>">
+	       					<img src="public/frontend/images/b_drop.png" data-toggle="tooltip" data-placement="left" title="delete" />
+	       				</a>
+       				</td>
+		<?php endif; ?>
+			  
+		
 	</tr>
 <?php
 	}
--- application/views/frontend/modules/boxtin.php
+++ application/views/frontend/modules/boxtin.php
@@ -4,7 +4,12 @@
 	</div>
 </div>
 <div class="boxtin">
-<div class="col-md-3"> 
+<?php 
+	$result = count($post);
+	if($result>=4):
+?>
+<div>
+	<div class="col-md-3"> 
         <a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[0]['idpost'] ?>">
         <img src="public/frontend/images/tinvan1.jpg" class="img-thumbnail" alt="moinhat1" width="100%"/>
         </a>
@@ -12,30 +17,36 @@
 		<a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[0]['idpost'] ?>"><?php echo $post[0]["title"];?></a>
 		</div>
 	</div>
+	
 	<div class="col-md-3"> 
-        <a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[0]['idpost'] ?>">
+        <a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[1]['idpost'] ?>">
         <img src="public/frontend/images/tinvan2.jpg" class="img-thumbnail" alt="moinhat1" width="100%"/>
         </a>
 	<div style="text-align:center">
 		<a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[1]['idpost'] ?>"><?php echo $post[1]["title"];?></a>
-	</div>                    
-</div>
+		</div>                    
+	</div>
+	
 	<div class="col-md-3"> 
-	<a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[0]['idpost'] ?>">
+	<a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[2]['idpost'] ?>">
 	<img src="public/frontend/images/tinvan3.jpg" class="img-thumbnail" alt="moinhat1" width="100%"/>
 	</a>
 		<div style="text-align:center">
 			<a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[2]['idpost'] ?>"><?php echo $post[2]["title"];?></a>
 		</div>                    
 	</div>
+
 	<div class="col-md-3"> 
-	<a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[0]['idpost'] ?>">
+	<a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[3]['idpost'] ?>">
 	<img src="public/frontend/images/tinvan4.jpg" class="img-thumbnail" alt="moinhat1" width="100%"/>
 	</a>
 		<div style="text-align:center">
 			<a href="<?php echo "http://localhost/studentneed/hientin/show/".$post[3]['idpost'] ?>"><?php echo $post[3]["title"];?></a>
 		</div>                    
 	</div>
-                
+</div>
+<?php else:?>
+<h1>                   </h1>
+<?php endif;?>
             
 </div>
