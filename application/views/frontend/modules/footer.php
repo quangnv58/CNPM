f69a186c051050d01ed63c18151cb6bbb5db0d2b
quangnv58 <quang0915057307@gmail.com>
2015-03-20 00:24:13 +0700

    Update footer.php

--- application/views/frontend/modules/footer.php
+++ application/views/frontend/modules/footer.php
@@ -1,30 +1,21 @@
-<div class="footer">
-	<div class="col-md-6">
-		<div class="embed-responsive embed-responsive-16by9">
-        	<div class="jumbotron">
-  				<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/AHrUsRVE_2Y" autoplay></iframe>
-                <div class="pull-bottom">
-                <p>Bạn có thích video này ?</p>
-                <div class="radio">
-                    <label><input type="radio" name="optradio" />Yes</label>
-                </div>
-                <div class="radio">
-                    <label><input type="radio" name="optradio"/>No</label>
-                </div>
-             </div>
-            </div>
-            
-		</div>
-    </div>
-    <div class="col-md-6 pull-right">
-        <div class="jumbotron">
-        	<h1>Love in work</h1>
-            <p>1.Làm điều bạn thích là tự do. Thích điều bạn làm là hạnh phúc.</p>
-            <p>2.Người phụ nữ có thể tự tạo công việc cho mình là người phụ nữ sẽ có được danh vọng và vận mệnh.</p>
-            <p>3.Khi bạn yêu thích công việc của mình, ngày nào cũng là ngày lễ.</p>
+
+<div>
+</br></br>
+    <div class="col-md-12 footer1">
+        <div class="col-md-6">
+            <h4>Contact us</h4>
+            <p>Phòng 314B kí túc xá ngoại ngữ</p>
+            <p>Phone:0123456789</p>
+        </div>
+        <div class="col-md-6">
+            <h4>Follow us</h4>
+            <a href="https://www.facebook.com/nguyen.quang.9028194"><img  src="public/frontend/css/fb.png" alt="fb" width="40" height="40"/></a>
+            <a href="https://www.facebook.com/nguyen.quang.9028194"><img  src="public/frontend/css/yt.png" alt="yt" width="40" height="40"/></a>
         </div>
     </div>
-    <div class="col-md-12">
-    <p style="text-align:center">Copy Right by 111B-IT</p>
+    
+    <div class="col-md-12 bq">
+        <p>Copyright by 111B-IT</p>
     </div>
+    
 </div>
