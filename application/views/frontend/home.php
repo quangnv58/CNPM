f661caff96d67d604958291720a6bbf7e605fece
quangnv58 <quang0915057307@gmail.com>
2015-03-20 00:24:47 +0700

    Update home.php

--- application/views/frontend/home.php
+++ application/views/frontend/home.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html >
+<html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -9,27 +9,28 @@
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
   <link href=" <?php echo base_url();?> public/frontend/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
   <link href="public/frontend/css/css/style.css" rel="stylesheet" type="text/css" />
-
+  <link rel="stylesheet" type="text/css" href="public/frontend/css/styleq.css">
   <title>Sinh viên cần</title>
 </head>
+
+<body>
 <div class="container-fluid">
-	<div class="mainmenu">
+  <div class="mainmenu">
     <?php $this->load->view('frontend/modules/menu.php');?>
   </div>
   <div class="container">
-  	<?php
-		if(isset($pages)&&isset($cat)){
-			$this->load->view("frontend/components/".$pages."/".$cat);
-			$this->load->view('frontend/modules/footer.php');
-		}
-		else{ 
-			$this->load->view('frontend/modules/slide.php');
-			$this->load->view('frontend/modules/boxtin.php');
-    		$this->load->view('frontend/modules/footer.php');
-		}
-	?>		
+    <?php
+    if(isset($pages)&&isset($cat)){
+      $this->load->view("frontend/components/".$pages."/".$cat);
+      $this->load->view('frontend/modules/footer.php');
+    }
+    else{ 
+      $this->load->view('frontend/modules/slide.php');
+      $this->load->view('frontend/modules/boxtin.php');
+      $this->load->view('frontend/modules/footer.php');
+    }
+  ?>    
   </div>
 </div><!--/*end container*/-->
-<body>
 </body>
 </html>
