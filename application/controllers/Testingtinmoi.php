4d64e561ddde35d730930a614097d4810cca1379
quangnv58 <quang0915057307@gmail.com>
2015-04-19 11:00:08 +0700

    testingsignup
    testingsignup

---
+++ application/controllers/Testingtinmoi.php
@@ -0,0 +1,29 @@
+<?php
+
+class Testpost extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('unit_test');
+		$this->load->model("frontend/mpost");
+	}
+	public function index()
+	{
+		$data=array(
+			'user'=>"admin",
+			'password'=>"123456"
+			);
+		$data2=array(
+			'user'=>"admin",
+			'password'=>"123456"
+			);
+		$this->mpost->insert($data);
+		 $test=$query->result_array();
+		$expected_result = $data;
+		$test_name = 'test signup';
+		$this->unit->run($test, $expected_result, $test_name);	
+		$this->unit->run($test, $expected_result, "test2");	
+		$this->load->view("test");
+	}
+}
