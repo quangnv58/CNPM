2a1bd683f2d4032270f6cf98b8c11167cbe82ae3
Tran Tien <manhtien12121995@gmail.com.vn>
2015-04-29 21:22:09 +0700

    Done --- Style Controller
    9h22 29/4

--- application/controllers/loginacc.php
+++ application/controllers/loginacc.php
@@ -1,21 +1,20 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Loginacc extends CI_Controller 
-{
+class Loginacc extends CI_Controller {
 	function __construct()
 	{
 		 parent::__construct();
 		$this->load->model("frontend/mmenu");
 		$this->load->model("frontend/mpost");
-		$this->data['menu']=$this->mmenu->menu_by(1,0);
-		$this->data['pages']='dangnhap';
+		$this->data['menu'] = $this->mmenu->menu_by(1,0);
+		$this->data['pages'] = 'dangnhap';
 		$this->load->library("session");
 		$this->load->library("facebook",array(
-			"appId"=>"1567383593519565",
-			"secret"=>"8e12a6ee40251675e9e965448edf76f1"
+			"appId"  => "1567383593519565",
+			"secret" => "8e12a6ee40251675e9e965448edf76f1"
 		));
-		$this->uid= $this->facebook->getUser();
-    	$this->access_token=$this->facebook->getAccessToken();
+		$this->uid = $this->facebook->getUser();
+    	$this->access_token = $this->facebook->getAccessToken();
 		$this->facebook->setAccessToken($this->access_token);
 		$this->load->model('frontend/user','',TRUE);
 		$this->load->library('form_validation');
@@ -25,10 +24,11 @@ class Loginacc extends CI_Controller
 	function index()
 	{
 		$this->load->helper(array('form'));
-		$this->data['cat']='login_view';
+		$this->data['cat'] = 'login_view';
 		$this->load->view('frontend/home',$this->data);
 	}
 	
 }
-
+/* End of file */
+/* Location: ./application/controllers/Loginacc.php */
 ?>
--- application/controllers/verifylogin.php
+++ application/controllers/verifylogin.php
@@ -7,15 +7,15 @@
 		$this->load->model('frontend/user','',TRUE);
 		$this->load->model("frontend/mmenu");
 		$this->load->model("frontend/mpost");
-		$this->data['menu']=$this->mmenu->menu_by(1,0);
-		$this->data['pages']='dangnhap';
+		$this->data['menu'] = $this->mmenu->menu_by(1,0);
+		$this->data['pages'] = 'dangnhap';
 		$this->load->library("session");
 		$this->load->library("facebook",array(
-			"appId"=>"1567383593519565",
-			"secret"=>"8e12a6ee40251675e9e965448edf76f1"
+			"appId"  => "1567383593519565",
+			"secret" => "8e12a6ee40251675e9e965448edf76f1"
 		));
-		$this->uid= $this->facebook->getUser();
-		$this->access_token=$this->facebook->getAccessToken();
+		$this->uid = $this->facebook->getUser();
+		$this->access_token = $this->facebook->getAccessToken();
 		$this->facebook->setAccessToken($this->access_token);
 		$this->load->model('frontend/user','',TRUE);
 		$this->load->library('form_validation');
@@ -34,7 +34,7 @@
 		if($this->form_validation->run() == FALSE)
 		{
 			//Field validation failed.  User redirected to login page
-			$this->data['cat']='login_view';
+			$this->data['cat'] = 'login_view';
 			$this->load->view('frontend/home',$this->data);
 		}
 		else
@@ -47,10 +47,10 @@
 	function check_database($password)
 	{
 		//Field validation succeeded.  Validate against database
-		$user=$this->input->post('user');
+		$user = $this->input->post('user');
 		
 		//query the database
-		$result=$this->user->login($user, $password);
+		$result = $this->user->login($user, $password);
 		
 		if($result)
 		{
@@ -58,8 +58,8 @@
 			foreach($result as $row)
 			{
 				$sess_array = array(
-					'opentid'=>$row->opentid,
-					'user'=>$row->user
+					'opentid' => $row->opentid,
+					'user' => $row->user
 				);
 				$this->session->set_userdata('logged_in', $sess_array);
 			}
@@ -72,4 +72,6 @@
 		}
 	}
 }
+/* End of file */
+/* Location: ./application/controllers/VerifyLogin.php */
 ?>
