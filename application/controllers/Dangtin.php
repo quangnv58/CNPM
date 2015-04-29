a16a2650bf4ac79493111fbe19a1012eb242c1fc
Tran Tien <manhtien12121995@gmail.com.vn>
2015-04-29 21:07:03 +0700

    Style File Name
    29/4

--- application/controllers/Dangtin.php
+++ application/controllers/Dangtin.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Dang_tin extends CI_Controller 
+class Dangtin extends CI_Controller 
 {
 	public function __construct()
     {
