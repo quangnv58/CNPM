bb36aa320e3331e8c97225c340ae75fc0e998243
Tran Tien <manhtien12121995@gmail.com.vn>
2015-04-30 22:13:47 +0700

    Done --- Style Model

--- application/models/frontend/mmenu.php
+++ application/models/frontend/mmenu.php
@@ -1,6 +1,7 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Mmenu extends CI_Model{
-	public function menu_by($level,$parent_id){
+	public function menu_by($level,$parent_id)
+	{
 		$table=$this->db->dbprefix('menu');	
 		$this->db->where('level',$level);
 		$this->db->where('parent_id',$parent_id);
@@ -10,5 +11,6 @@ class Mmenu extends CI_Model{
 		return $query->result_array();
 	}
 }
-
+/* End of file */
+/* Location: ./application/models/mmenu.php */
 ?>
--- application/models/frontend/mpost.php
+++ application/models/frontend/mpost.php
@@ -1,16 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class Mpost extends CI_Model{
-	function insert($data){ 
+	function insert($data)
+	{ 
 		$this->db->insert("post",$data); 
 		return $this->db->insert_id(); 
     }
-	public function post_by(){
+	public function post_by()
+	{
 		$table=$this->db->dbprefix('post');	
 		$this->db->order_by('title','asc');
 		$query=$this->db->get($table);
 		return $query->result_array();
 	}
-
 }
-
+/* End of file */
+/* Location: ./application/models/mpost.php */
 ?>
