d68eb28883d52548fc9fbc77958dcb8140567abf
tientm58 <manhtien12121995@gmail.com>
2015-05-19 01:05:04 +0700

    Test singUp bản 1
    Fail... Check các trường

---
+++ application/controllers/testingunit/Test_signUp1.php
@@ -0,0 +1,34 @@
+<?php
+class Test_signUp1 extends CI_Controller{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('unit_test');
+		$this->load->model("mlogin");
+	}
+	public function index()
+	{
+		$data = array (
+			'name' 	   => "",
+			'user' 	   => "manhtien1212",
+			'password' => "12121995",
+			'email'    => "manhtien121219952gmail.com",
+			'field'    => "IT"
+		);	
+		$this->mlogin->insert($data);
+		$query = $this->db->get_where('user', array('user' => "manhtien1212"));
+        $row = $query->row();
+		$data_result = array(
+			'name'     => $row->name, 
+			'user'     => $row->user,  
+			'password' => $row->password,
+			'email'    => $row->email, 
+			'field'    => $row->field, 
+		);
+		$test_name = 'test add account to database';
+		$this->unit->run($data_result, $data, $test_name);	
+		$this->load->view("test");
+	}
+}
+/* End of file test_sinUp.php */
+/* Location: ./application/controllers/testingunit/Test_signUp.php */
