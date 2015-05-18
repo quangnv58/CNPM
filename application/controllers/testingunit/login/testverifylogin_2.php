da2cdc823ffacd241d0f65e0f7048506faaae797
trongdd58 <trongdd95@gmail.com>
2015-05-19 00:57:09 +0700

    them classdiagram va testlogin

---
+++ application/controllers/testingunit/login/testverifylogin_2.php
@@ -0,0 +1,27 @@
+<?php
+class Testverifylogin_2 extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('unit_test');
+		$this->load->model('frontend/user','',TRUE);
+		$this->load->model("frontend/mmenu");
+		$this->load->library("session");
+		$this->load->library("facebook",array(
+			"appId"=>"1567383593519565",
+			"secret"=>"8e12a6ee40251675e9e965448edf76f1"
+		));
+		$this->access_token=$this->facebook->getAccessToken();
+		$this->load->library('form_validation');
+	}
+	public function index()
+	{
+		$data=false;
+		$user=$this->input->post('trong');
+		$data_result=$this->user->login($user, "123");
+		$test_name='test function check_database';
+		$this->unit->run($data_result, $data, $test_name);	
+		$this->load->view("test");
+	}
+}
